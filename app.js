6611a97cc8fb8cc526788eecca9be58ef791727b
yujin kim <koiforever0526@gmail.com>
2025-03-13 11:09:20 +0900

    save

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ const OpenAI = require("openai");
 const app = express();
 // app.use(cors());
 app.use(
-  cors(
-    cors({
-      origin: "*",
-    })
-  )
+  cors({
+    origin: "*",
+  })
 );
 app.use(express.json()); // 파라미터가 apllication/json 형태로 들어오면 파싱
 app.use(express.urlencoded({ extended: true })); // 파라미터가 application/x-www-form-urlencoded 형태로 들어오면 파싱
